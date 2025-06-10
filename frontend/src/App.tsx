3de155a63c77069eb1e99fa857453111ca3ecfb1
mirzviz <mirzviz@gmail.com>
2025-06-10 18:09:24 +0300

    fix npm build

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import VideoUpload from './components/VideoUpload';
